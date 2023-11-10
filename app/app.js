0410c8a42e5df1c9fcde29b9df987393470348d3
shahin <shahin.aboulhassanibavili2021@campus-eni.fr>
2023-11-10 17:17:58 +0100

    Union Types

--- app/app.js
+++ app/app.js
@@ -17,7 +17,6 @@ var Role;
     Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
     Role[Role["AUTHOR"] = 2] = "AUTHOR";
 })(Role || (Role = {}));
-;
 var person = {
     name: 'Maximilian',
     age: 30,
@@ -27,3 +26,17 @@ var person = {
 if (person.role === Role.AUTHOR) {
     console.log(person.role);
 }
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+var combinedAge = combine(25, 15);
+console.log(combinedAge);
+var combinedName = combine("Max", "Anna");
+console.log(combinedName);
--- app/app.ts
+++ app/app.ts
@@ -26,3 +26,21 @@ const person = {
 if (person.role === Role.AUTHOR) {
     console.log(person.role);
 }
+
+function combine(input1: number | string, input2: number | string){
+    let result;
+    if (typeof input1 === "number" && typeof input2 === "number") {
+         result = input1 + input2;
+    }else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+
+const combinedAge = combine(25, 15);
+
+console.log(combinedAge);
+
+const combinedName = combine("Max", "Anna");
+
+console.log(combinedName);
