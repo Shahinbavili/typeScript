59429db10f28a80a01c5c34cdda0e555f62944e3
shahin <shahin.aboulhassanibavili2021@campus-eni.fr>
2023-11-14 18:49:52 +0100

    before Literal Types

--- app/app.js
+++ app/app.js
@@ -26,17 +26,24 @@ var person = {
 if (person.role === Role.AUTHOR) {
     console.log(person.role);
 }
-function combine(input1, input2) {
+function combine(input1, input2, resultConversion) {
     var result;
-    if (typeof input1 === "number" && typeof input2 === "number") {
-        result = input1 + input2;
+    if (typeof input1 === "number" && typeof input2 === "number" || resultConversion === 'as-number') {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
-    return result;
+    if (resultConversion === 'as-number') {
+        return +result;
+    }
+    else {
+        return result.toString();
+    }
 }
-var combinedAge = combine(25, 15);
+var combinedAge = combine(30, 26, 'as-number');
 console.log(combinedAge);
-var combinedName = combine("Max", "Anna");
+var combinedStringAge = combine('30', '26', 'as-number');
+console.log(combinedStringAge);
+var combinedName = combine("Max", "Anna", 'as-text');
 console.log(combinedName);
--- app/app.ts
+++ app/app.ts
@@ -13,7 +13,7 @@ let resultPhrase = 'Result is: ';
 
 add(number1, number2, printResult, resultPhrase);
 
-enum Role  {ADMIN, READ_ONLY,AUTHOR}
+enum Role {ADMIN, READ_ONLY, AUTHOR}
 
 const person = {
     name: 'Maximilian',
@@ -27,20 +27,29 @@ if (person.role === Role.AUTHOR) {
     console.log(person.role);
 }
 
-function combine(input1: number | string, input2: number | string){
+function combine(input1: number | string, input2: number | string, resultConversion: string) {
     let result;
-    if (typeof input1 === "number" && typeof input2 === "number") {
-         result = input1 + input2;
-    }else {
+    if (typeof input1 === "number" && typeof input2 === "number" || resultConversion === 'as-number') {
+        result = +input1 + +input2;
+    } else {
         result = input1.toString() + input2.toString();
     }
-    return result;
+
+    if (resultConversion === 'as-number') {
+        return +result;
+    } else {
+        return result.toString();
+    }
 }
 
-const combinedAge = combine(25, 15);
+const combinedAge = combine(30, 26, 'as-number');
 
 console.log(combinedAge);
 
-const combinedName = combine("Max", "Anna");
+const combinedStringAge = combine('30', '26', 'as-number');
+
+console.log(combinedStringAge);
+
+const combinedName = combine("Max", "Anna", 'as-text');
 
 console.log(combinedName);
